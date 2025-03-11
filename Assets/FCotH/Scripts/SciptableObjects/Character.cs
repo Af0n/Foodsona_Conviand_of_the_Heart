d56c87ecec71a529ffb7657110d13695aa6c3dfa
AndAgGit <afon.cyffrous@gmail.com>
2025-03-11 14:54:00 -0700

    Added character tostring

--- Assets/FCotH/Scripts/SciptableObjects/Character.cs
+++ Assets/FCotH/Scripts/SciptableObjects/Character.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 public class Character : ScriptableObject
 {
     public string Name;
+    public int Level;
     public CharacterStats Stats;
+
+    public override string ToString()
+    {
+        string result = "";
+        result += "Name: " + Name;
+        result += "\nLevel: " + Level;
+        result += "\nStats";
+        result += "\nHP: " + LevelCalculator.GetStatLevel(Stats.BaseHP, Stats.BaseHPGrowth, Level);
+        result += "\nATK: " + LevelCalculator.GetStatLevel(Stats.BaseATK, Stats.BaseATKGrowth, Level);
+        result += "\nSP: " + LevelCalculator.GetStatLevel(Stats.BaseSP, Stats.BaseSPGrowth, Level);
+        result += "\nARM: " + LevelCalculator.GetStatLevel(Stats.BaseARM, Stats.BaseARMGrowth, Level);
+        result += "\nPLAT: " + LevelCalculator.GetStatLevel(Stats.BasePLAT, Stats.BasePLATGrowth, Level);
+        return base.ToString();
+    }
 }
